5d7a25a5e3e6eae22505dd6756e4f501d97950c2
Michael Perry <michaeleperr@gmail.com>
2021-08-12 18:26:36 -0400

    0.24 Updated tab icons and added a new tab labelled notes.

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ import {
   IonTabs,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle, book, calculator, time, business } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Tab4 from './pages/Tab4';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -47,22 +48,29 @@ const App: React.FC = () => (
           <Route path="/tab3">
             <Tab3 />
           </Route>
+          <Route path="/tab4">
+            <Tab4 />
+          </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
+            <IonIcon icon={time} />
             <IonLabel>Cashiers</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
+            <IonIcon icon={calculator} />
             <IonLabel>Lottery</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
+            <IonIcon icon={business} />
             <IonLabel>Office</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon icon={book} />
+            <IonLabel>Notes</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
---
+++ src/pages/Tab4.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage,  IonTitle, IonLabel, IonToolbar, IonInput, IonCheckbox, IonItem, IonButton, IonTextarea, IonGrid, IonRow, IonCol, IonModal, IonItemGroup, IonItemDivider, IonSegment} from '@ionic/react';
+import './Tab4.css';
+import { forceUpdate } from 'ionicons/dist/types/stencil-public-runtime';
+
+const Tab4: React.FC = () => {
+
+  
+
+
+
+  
+
+  return (
+    
+    <IonPage>
+      <IonHeader>
+        <IonToolbar class='textback4'>
+          <IonTitle class='titlefont4'>Notes</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent class='content4' fullscreen>
+      
+      </IonContent>
+    </IonPage>
+  );
+  
+};
+
+
+
+export default Tab4;
