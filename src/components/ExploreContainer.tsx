6e88631c3c21d08f66d398c824f7ecc4669f286f
Michael Perry <michaeleperr@gmail.com>
2021-08-12 16:52:25 -0400

    Added credit in console

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -9,6 +9,7 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
     <div className="container">
       <strong>{name}</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      {console.log("Made with love <3 Michael Perry")}
     </div>
   );
 };
